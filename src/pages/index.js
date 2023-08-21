b7ae0d3a2a3f6f3cd91b0327766b0341b372b40e
abir14016 <abir14016@gmail.com>
2023-08-21 12:58:55 +0600

    [DONE] fetch data with getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
 
 //cloned starter pack
-const HomePage = () => {
+const HomePage = ({ allNews }) => {
+  console.log(allNews)
   return (
     <>
       <Head>
@@ -24,3 +25,13 @@ export default HomePage;
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
 };
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news")
+  const data = await res.json();
+  // console.log(data)
+  return {
+    props: { allNews: data }
+  }
+}
