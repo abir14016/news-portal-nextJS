f36f7756e15dda5de1c7ff808db94401c889571a
abir14016 <abir14016@gmail.com>
2023-08-22 19:01:24 +0600

    [DONE] connect nextJS server with mongodb and get data

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -1,6 +1,33 @@
-const handler = (req, res) => {
-    res.send({ message: "erver created" })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://news-portal-admin:kRMrldEynB6i3N3u@cluster0.boat8.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run(req, res) {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        console.log("database connection established");
+
+        const newsCollection = client.db("news-portal").collection("news")
+
+        if (req.method === "GET") {
+            const news = await newsCollection.find({}).toArray();
+            res.send({ message: "success", statusCode: 200, data: news })
+        }
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 
-
-export default handler;
+export default run
